c800ac62089fb1c6d436495688b0a5877f116047
lychee <876505905@qq.com>
2018-01-04 17:16:43 +0800

    修改admin报错

--- backend/components/AccessControl.php
+++ backend/components/AccessControl.php
@@ -26,7 +26,7 @@ class AccessControl extends \yii\filters\AccessControl {
         $actionId = $action->getUniqueId();
         foreach ($this->rules as $i => $rule) {
             if(in_array($action->id, $rule->actions)) break;
-            if(Yii::$app->user->identity->username == 'admin') {
+            if(!Yii::$app->user->isGuest && Yii::$app->user->identity->username == 'admin') {
                 $this->rules[] = Yii::createObject(array_merge($this->ruleConfig, [
                     'actions' => [$action->id],
                     'allow' => true,
