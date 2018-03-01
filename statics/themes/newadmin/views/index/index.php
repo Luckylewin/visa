12b7f850fa8fe01dbf8d15d5cbf61991a00facb7
Luckylewin <876505905@qq.com>
2018-03-01 18:15:58 +0800

    优化首页按钮

--- statics/themes/newadmin/views/index/index.php
+++ statics/themes/newadmin/views/index/index.php
@@ -2,8 +2,13 @@
     <div class="jumbotron">
         <h2>签证订单管理系统</h2>
         <p></p>
-        <p><a class="btn btn-primary btn-lg" href="<?= \yii\helpers\Url::to(['order/create']) ?>" role="button">
-                录入订单</a>
+        <p>
+            <a class="btn btn-primary btn-lg" href="<?= \yii\helpers\Url::to(['order/create']) ?>" role="button">
+                录入订单
+            </a>
+            <a class="btn btn-info btn-lg" href="<?= \yii\helpers\Url::to(['excel/import']) ?>" role="button">
+                导入Excel
+            </a>
         </p>
     </div>
 </div>
