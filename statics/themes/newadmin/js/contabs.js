57e4d464f3c62148b0f130ed4b2f799804d25a9d
Luckylewin <876505905@qq.com>
2018-07-06 16:43:12 +0800

    下拉框的问题

--- statics/themes/newadmin/js/contabs.js
+++ statics/themes/newadmin/js/contabs.js
@@ -3,7 +3,7 @@
  *   H+ - 后台主题UI框架
  *   version 4.9
  *
-*/
+ */
 
 $(function () {
     //计算元素集合的总宽度
@@ -111,7 +111,9 @@ $(function () {
         }
     });
 
+
     function menuItem() {
+
         // 获取标识数据
         var dataUrl = $(this).attr('href'),
             dataIndex = $(this).data('index'),
@@ -144,20 +146,34 @@ $(function () {
             $('.J_menuTab').removeClass('active');
 
             // 添加选项卡对应的iframe
-            var str1 = '<iframe class="J_iframe" name="iframe' + dataIndex + '" width="100%" height="100%" src="' + dataUrl + '" frameborder="0" data-id="' + dataUrl + '" seamless></iframe>';
+            var str1 = '<iframe class="J_iframe" id="iframe' + dataIndex + '" name="iframe' + dataIndex + '" width="100%" height="100%" src="' + dataUrl + '" frameborder="0" data-id="' + dataUrl + '" seamless onload="if(this.contentWindow.location.href != this.src){this.ischange=1}" ischange="0"></iframe>';
             $('.J_mainContent').find('iframe.J_iframe').hide().parents('.J_mainContent').append(str1);
 
-            //显示loading提示
-//            var loading = layer.load();
-//
-//            $('.J_mainContent iframe:visible').load(function () {
-//                //iframe加载完成后隐藏loading提示
-//                layer.close(loading);
-//            });
+            /* //显示loading提示
+             var loading = layer.load();
+
+             $('.J_mainContent iframe:visible').load(function () {
+             //iframe加载完成后隐藏loading提示
+             layer.close(loading);
+             });*/
+
             // 添加选项卡
             $('.J_menuTabs .page-tabs-content').append(str);
             scrollToTab($('.J_menuTab.active'));
+        } else {
+            // 重新打开url
+            var name = 'iframe' +  dataIndex;
+
+            $('.J_iframe').each(function(k,v) {
+
+                if (v.name === name && ['undefined', 1].indexOf(v.ischange) !== -1) {
+                    v.ischange = 0;
+                    v.src = dataUrl;
+                }
+            });
+
         }
+
         return false;
     }
 
