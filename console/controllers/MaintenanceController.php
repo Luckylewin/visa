be6697e170e76ec056f40bd5d02bad65dcec8ce0
Luckylewin <876505905@qq.com>
2018-08-21 10:54:48 +0800

    去掉迁移

--- console/controllers/MaintenanceController.php
+++ console/controllers/MaintenanceController.php
@@ -14,18 +14,5 @@ use yii\console\Controller;
 class MaintenanceController extends Controller
 {
 
-    public function updateAction()
-    {
-        $orders = Order::find()->all();
-        if (!empty($orders)) {
-            foreach ($orders as $order) {
-                if (!empty($order->operator_id)) {
-                    $order->creator_id = $order->operator_id;
-                    $order->save(false);
-                }
-            }
-        }
-
-        echo 'ok';
-    }
+   
 }
